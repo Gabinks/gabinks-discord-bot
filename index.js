6e0326e73251db3ebd69ee12575cd703e0ce96d6
LeDiablee <47298993+LeDiablee@users.noreply.github.com>
2021-07-17 20:51:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ Client.on("message", msg => {
             msg.author.send("Pour report un joueur veuillez préciser le nom du joueur et la raison Exemple : !/report @Gabinks Insule");
         }else{
             const args = msg.content.split(" ");
-            const messageContent = msg.content.slice(args[2].length + 1);
+            const messageContent = msg.content.slice(args[1].length);
 
             channel = Client.channels.cache.get('866009504054509578');
 
