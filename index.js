e3965b8d3ad8a8b389a9b07d71b6b38cf57313c8
LeDiablee <47298993+LeDiablee@users.noreply.github.com>
2021-07-18 13:54:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -48,13 +48,18 @@ Client.on("message", msg => {
 
     if(msg.content.startsWith(prefix + "report")){
         let mention = msg.mentions.members.first();
-        let args = msg.content.split(' ').splice(2).join(' ');
 
-        if (mention == undefined){ 
-            msg.author.send("Pour report un joueur veuillez préciser le nom du joueur et la raison Exemple : !/report @Gabinks Insule");
+        if(mention == undefined){
+            if(args == undefined){
+                 msg.author.send("Pour report un joueur veuillez préciser le nom du joueur et la raison Exemple : !/report @Gabinks Insule");
+            }
+        }else{
+            let args = msg.content.split(' ').splice(2).join(' ');
+
+            channel = Client.channels.cache.get('866009504054509578');
+
+            channel.send("<@" + msg.author.id + "> a **report** <@" + mention.id + "> pour " + "__"+args+"__");
         }
-        channel = Client.channels.cache.get('866009504054509578');
-        channel.send("<@" + msg.author.id + "> a **report** <@" + mention.id + "> pour " + "__"+args+"__");
     }
 
     if(msg.member.hasPermission("ADMINISTRATOR")){
